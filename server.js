d4f09324bb63a851f001dd166a6fa3ac4c9229a6
sephiroth7712 <roshanjames7@gmail.com>
2019-03-02 12:55:41 +0530

    Added : Updating ledger with payment bill details

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var User = require('./models/user');
 var Track = require('./models/track');
 var userRoutes   = require('./routes/image');
 
+var ip =  "http://192.168.43.229:3000";
+
 var Request = require("request");
 
 var cors= require('cors');
@@ -59,6 +61,122 @@ mongoose.connect(conString,(err) => {
     });
 });
 
+//uploading payment details of Raw materials from supplier to trader on the blockchain
+app.post('/paymentUploadRaw',function(req,res){
+    try{
+        console.log(req.body);
+        var toSend = {
+            "$class" :"org.network.tracktrace.paymentRaw",
+            "trader" : "resource:org.network.tracktrace.Trader#"+req.body.traderID,
+            "amount" : req.body.amount,
+            "qty" : req.body.quantity,
+            "rawbatchId" : req.body.batchID
+        };
+        console.log(typeof toSend);
+        var options = { 
+            method: 'POST',
+            url: ip+"/api/paymentRaw/",
+            headers:{ 
+                'Content-Type': 'application/json' },
+            body: toSend,
+            json: true };
+
+        Request(options, function (error, response, body) {
+            if (error){
+                throw new Error(error);
+            } 
+          
+            console.log(body);
+        });
+
+        res.sendStatus(200);    
+    }
+    catch(error){
+        // console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/packingUploadRaw',function(req,res){
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+app.post('/paymentUploadTrader',function(req,res){
+    try{
+        console.log(req.body);
+        var toSend = {
+            "$class" : "org.network.tracktrace.paymentTrade",
+            "trader": "resource:org.network.tracktrace.Factory"+req.body.factoryID, 
+            "amount": req.body.amount, 
+            "qty": req.body.quantity, 
+            "rawbatchId": req.body.batchID
+        };
+        Request.post(ip+"/api/paymentTrade",(error,success)=>{
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(success);
+            }
+        });
+        res.sendStatus(200);
+    }
+    catch(error){
+        res.sendStatus(500);
+    }
+});
+
+app.post('/paymentUploadDistributor',function(req,res){
+    try{
+        console.log(req.body);
+        var toSend = {
+            "$class" : "org.network.tracktrace.paymentTrade",
+            "trader": "resource:org.network.tracktrace.Retailer"+req.body.retailerID, 
+            "amount": req.body.amount, 
+            "qty": req.body.quantity, 
+            "rawbatchId": req.body.batchID
+        };
+        Request.post(ip+"/api/paymentTrade",(error,success)=>{
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(success);
+            }
+        });
+        res.sendStatus(200);
+    }
+    catch(error){
+        res.sendStatus(500);
+    }
+});
+
+
+app.post('/paymentUploadFactory',function(req,res){
+    try{
+        console.log(req.body);
+        var toSend = {
+            "$class" : "org.network.tracktrace.paymentJam",
+            "trader": "resource:org.network.tracktrace.Distributor"+req.body.distributorID, 
+            "amount": req.body.amount, 
+            "qty": req.body.quantity, 
+            "rawbatchId": req.body.batchID
+        };
+        Request.post(ip+"/api/paymentTrade",(error,success)=>{
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(success);
+            }
+        });
+        res.sendStatus(200);
+    }
+    catch(error){
+        res.sendStatus(500);
+    }
+});
 
 // //logging arduino data
 app.post('/livetracking',function(req,res){
@@ -114,12 +232,12 @@ app.get('/test', (req,res)=>{
 });
 
 app.get('/test2', (req,res) =>{
-  Request.get("http://192.168.43.229:3000/api/Grower/666",(error,response,body)=>{
+  Request.get(ip+"/api/Grower/666",(error,response,body)=>{
       if(error){
           return console.log(error);
       }
   });
-})
+});
 
 // launch ======================================================================
 app.listen(port);
